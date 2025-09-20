d291690f36796ebc6447cf41282246a9917004db
biplab mahanty <bmahanty501@gmail.com>
2025-09-20 15:35:17 +0530

    initial commit

---
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -0,0 +1,8 @@
+// Reference the example args file: https://github.com/scaffold-eth/create-eth-extensions/blob/example/extension/packages/nextjs/components/Header.tsx.args.mjs
+// Reference the template file that will use this file: https://github.com/scaffold-eth/create-eth/blob/main/templates/base/packages/nextjs/components/Header.tsx.template.mjs
+
+// Default args:
+export const preContent = "";
+export const extraMenuLinksObjects = [];
+export const logoTitle = "Scaffold-ETH";
+export const logoSubtitle = "Ethereum dev stack";
